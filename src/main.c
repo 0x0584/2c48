b5f4d3310f591b3cc3b32343b408ac4bae976d31
Anas Rchid <anasrchid@users.noreply.github.com>
2016-01-27 14:08:04 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,8 +3,12 @@
 
 int main()
 {
+	/*Setup terminal settings*/
 	tcgetattr(STDIN_FILENO, &oldt);
     	newt = oldt;
     	newt.c_lflag &= ~(ICANON | ECHO);
     	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    	/*Restor terminal settings*/
+    	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return 0;
 }
