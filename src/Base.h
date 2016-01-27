29b8a9ab42243c579a64b6c543705185d1f852c0
Anas Rchid <anasrchid@users.noreply.github.com>
2016-01-27 13:54:35 +0000

    Update Base.h

--- src/Base.h
+++ src/Base.h
@@ -1,21 +1,33 @@
 #ifndef _BASE_H
  #define _BASE_H
 
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <math.h>
-  #include <time.h>
-  #include <ncurses.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <termios.h>
+ #include <time.h>
+ #include <unistd.h>
 
-#if CURSED
-  #define printf printw
-#else
-  #define printf printf
-#endif
-// D for Dimension
-#define Dim 4 
-#define GAMESIZE Dim*Dim
-
-static int Ts[GAMESIZE];
-
+  #define INVALID -1
+  #define UP       1
+  #define DOWN     2
+  #define RIGHT    3
+  #define LEFT     4
+   const long vals[] = {
+	   0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048
+   };
+   
+   const char *colors[] = {
+	   "31", "39", "33", "32", "34", "35", "36", "37", "91", "92", "93", "94"
+   };
+   #define D 4
+   struct GameStructure__Status {
+	   int grid[D][D];
+	   int moved;
+	   long score;
+	   long last_score;
+	   int blocks;
+   } game;
+   
+   struct termios oldt, newt;
 #endif
